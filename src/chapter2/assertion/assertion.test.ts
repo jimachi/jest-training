fafa14f83727a1c819ce947723ea4946ca4f9380
jimachi <jimaichiro@gmail.com>
2023-03-11 15:03:06 +0900

    エラーのテスト

--- src/chapter2/assertion/assertion.test.ts
+++ src/chapter2/assertion/assertion.test.ts
@@ -267,3 +267,30 @@ it('triggered by the scheduled pipeline', () => {
     })
   )
 })
+
+class User {
+  name: string
+  password: string
+  constructor({ name, password }: { name: string; password: string }) {
+    if (password.length < 6) {
+      throw new Error('The password length must be at least 6 characters.')
+    }
+    this.name = name
+    this.password = password
+  }
+}
+
+it('create a new user with a 6-character password', () => {
+  expect(new User({ name: 'hoge', password: '123456' })).toEqual({
+    name: 'hoge',
+    password: '123456',
+  })
+})
+
+it('throw Error when the length of password is less than 6', () => {
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow()
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow(Error)
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow(
+    'The password length must be at least 6 characters.'
+  )
+})
